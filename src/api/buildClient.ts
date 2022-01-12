13bcaa2840e5b2759f4621978a293734d524d7f5
Adam <adampapamast@hotmail.com>
2022-01-12 15:27:49 +0200

    remove: unecessary code

--- src/api/buildClient.ts
+++ src/api/buildClient.ts
@@ -2,20 +2,17 @@ import axios, { AxiosRequestHeaders } from "axios";
 import { NextPageContext } from "next";
 import { NextRequest } from "next/server";
 
-interface IBuildClient {
-  req?: NextRequest;
-}
-
 const buildClient = (ctx: NextPageContext) => {
   if (typeof window === "undefined") {
     // we are on server
-
-    console.log(ctx.req?.headers);
     return axios.create({
       baseURL: "http://localhost:4000",
       // TODO: check headers type missmatch nextjs/axios
       // @ts-ignore
-      headers: ctx.req.headers,
+      // headers: ctx.req.headers,
+      // headers: {
+      //   "Content-Type": "application/json",
+      // },
     });
   } else {
     // on browser
