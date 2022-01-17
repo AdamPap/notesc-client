1c5729a23d323959bd0f026aad90726f2cc95fab
Adam <adampapamast@hotmail.com>
2022-01-17 18:40:44 +0200

    add: List

--- custom-types.d.ts
+++ custom-types.d.ts
@@ -10,6 +10,15 @@ interface Board {
   id: number;
   title: string;
   content: string;
+  lists: List[];
+  createdAt: string;
+  updatedAt: string;
+}
+
+interface List {
+  id: number;
+  title: string;
+  content: string;
   createdAt: string;
   updatedAt: string;
 }
