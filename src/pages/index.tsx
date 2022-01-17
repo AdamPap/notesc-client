29c62f8fee3e0896759131e7fb921b9de2a6f583
Adam <adampapamast@hotmail.com>
2022-01-17 17:13:41 +0200

    add: basic styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Grid, GridItem, Link, useColorModeValue } from "@chakra-ui/react";
 import { NextPage, NextPageContext } from "next";
+import NextLink from "next/link";
 import buildClient from "../api/buildClient";
 
 interface BoardsIndexProps {
@@ -7,13 +8,27 @@ interface BoardsIndexProps {
 }
 
 const BoardsIndex: NextPage<BoardsIndexProps> = ({ boards }) => {
-  console.log(boards);
   return (
     <Box>
       <Box>Boards Index</Box>
-      {boards.map((board) => (
-        <Box key={board.id}>{board.title}</Box>
-      ))}
+      <Grid templateColumns="repeat(5, 1fr)" gap={6}>
+        {boards.map((board) => (
+          <GridItem w="100%" key={board.id}>
+            <Box bg={useColorModeValue("teal.500", "teal.300")}>
+              <NextLink
+                href="/boards/[boardId]"
+                as={`/boards/${board.id}`}
+                passHref
+              >
+                <Link>{board.title}</Link>
+              </NextLink>
+              <Box bg={useColorModeValue("teal.800", "teal.100")}>
+                {board.content}
+              </Box>
+            </Box>
+          </GridItem>
+        ))}
+      </Grid>
     </Box>
   );
 };
