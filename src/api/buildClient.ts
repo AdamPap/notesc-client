2fdc91eae583b474a247c22374bfa1b124e158a4
Adam <adampapamast@hotmail.com>
2022-01-20 20:47:13 +0200

    remove: headers

--- src/api/buildClient.ts
+++ src/api/buildClient.ts
@@ -1,6 +1,5 @@
-import axios, { AxiosRequestHeaders } from "axios";
+import axios from "axios";
 import { NextPageContext } from "next";
-import { NextRequest } from "next/server";
 
 const buildClient = (ctx: NextPageContext) => {
   if (typeof window === "undefined") {
@@ -10,9 +9,6 @@ const buildClient = (ctx: NextPageContext) => {
       // TODO: check headers type missmatch nextjs/axios
       // @ts-ignore
       // headers: ctx.req.headers,
-      // headers: {
-      //   "Content-Type": "application/json",
-      // },
     });
   } else {
     // on browser
