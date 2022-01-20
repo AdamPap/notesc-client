97b40c23bc2812ff3f4c82e135209ba671dc784b
Adam <adampapamast@hotmail.com>
2022-01-20 20:44:50 +0200

    update: styles

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,6 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Flex, IconButton, Text } from "@chakra-ui/react";
 import React from "react";
+import { MdModeEditOutline } from "react-icons/md";
 
 interface CardProps {
   cardTitle: string;
@@ -7,8 +8,21 @@ interface CardProps {
 
 export const Card: React.FC<CardProps> = ({ cardTitle }) => {
   return (
-    <Box rounded="md" bg="white" w="100%" py={2} px={3}>
-      <Text>{cardTitle}</Text>
+    <Box shadow="sm" rounded="md" bg="white" w="100%" p={1}>
+      <Flex role="group" position="relative" py={2} px={3}>
+        <Text>{cardTitle}</Text>
+        <IconButton
+          position="absolute"
+          top={0}
+          right={0}
+          opacity={0}
+          _groupHover={{ opacity: 1 }}
+          _hover={{ bg: "green.100" }}
+          size="xs"
+          aria-label="Edit Card"
+          icon={<MdModeEditOutline />}
+        />
+      </Flex>
     </Box>
   );
 };
